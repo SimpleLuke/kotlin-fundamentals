e6769f3ab9f5d829a814b6da3047f9aa411c89e7
Luke Lai <lukelai.dev@gmail.com>
2023-05-04 21:29:03 +0100

    Add revealSecret function

--- Functions/functions/src/main/kotlin/Main.kt
+++ Functions/functions/src/main/kotlin/Main.kt
@@ -9,4 +9,24 @@ fun main(args: Array<String>) {
         return "super$word"
     }
     println(superify(superify(superify(superify("cat")))))
+
+//    Define a function revealSecret which takes a passcode in argument, and returns a secret string "The cookies are in the desk drawer" only if the given passcode is the string "HUNGRY".
+//    If the given passcode is something else, the function should return null.
+//    (What should be the return value type of this function?)
+//    val secret = revealSecret("HUNGRY")
+//    println(secret) // "The cookies are in the desk drawer"
+//    val secret = revealSecret("Hi")
+//    println(secret) // null
+
+    fun revealSecret(passcode: String):String?{
+        if (passcode==="HUNGRY"){
+            return "The cookies are in the desk drawer"
+        }
+        return null
+    }
+
+    val secret1 = revealSecret("HUNGRY")
+    println(secret1) // "The cookies are in the desk drawer"
+    val secret2 = revealSecret("Hi")
+    println(secret2) // null
 }
